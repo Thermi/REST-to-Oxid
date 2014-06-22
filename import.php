35a452758c66d626934414ed0363f308db7ace0c
Thermi <noel@familie-kuntze.de>
2014-06-23 00:28:47 +0200

    import.php: Added the licence header and setting the category of the article

--- import.php
+++ import.php
@@ -1,17 +1,36 @@
 <?php
 
+/*
+ * Copyright (C) 2014 Noel Kuntze <noel@familie-kuntze.de>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 require dirname(__FILE__) . "/bootstrap.php";
 
 
-/* Importstart */
+/* You need to create a category beforehand and then get its ID by looking in the oxcategories table */
+/* The category ID has to be set here */
+$category = "15eb8d678953e37addc7451995279e57";
 
-/* Produkte abrufen */
+/* import start  */
+
+/* getting the products  */
 
 $productRequestURL = 'http://student.mi.hs-offenburg.de:8080/sqlrest/PRODUCT';
 $productResponse = file_get_contents($productRequestURL);
 $productXMLData = simplexml_load_string($productResponse);
 
-/* Benutzer abrufen */
+/* getting the users  */
 
 $customerRequestURL = 'http://student.mi.hs-offenburg.de:8080/sqlrest/CUSTOMER';
 $customerResponse = file_get_contents($customerRequestURL);
@@ -37,20 +56,39 @@ foreach ($productXMLData->PRODUCT as $entry) {
     $i = null;
     /* prepare statement */
     /* problem here: Generate an unique OXID for every article  */
-    $statement = $connection->prepare("INSERT INTO oxarticles (OXID, OXARTNUM, OXTITLE, OXPRICE, OXSUBCLASS) VALUES (?, ?, ?, ?, 'oxarticle')");
+    $statement = $connection->prepare("INSERT INTO oxarticles (OXID, OXARTNUM, OXTITLE, OXPRICE, OXSUBCLASS, OXSHOPID, OXSTOCK, OXTITLE_1) VALUES (?, ?, ?, ?, 'oxarticle', 'oxbaseshop', 1, ?)");
     if(!$statement) {
-        echo "Line 42: Prepare failed: (" . $connection->errno . ")" . $connection->error . PHP_EOL;
+        echo "Prepare failed: (" . $connection->errno . ")" . $connection->error . PHP_EOL;
         exit();
     }
 
     /* bind statement */
-    if(!$statement->bind_param("sisd", $RANDOMID, $ID, $NAME, $PRICE)) {
+    if(!$statement->bind_param("sisds", $RANDOMID, $ID, $NAME, $PRICE, $ID)) {
+        echo "Binding parameters failed: (" . $statement->errno . ") " . $statement->error . PHP_EOL;
+        exit();
+    }
+
+    if(!$statement->execute()) {
+        echo "Execute failed: (" . $statement->errno . ") " . $statement->error . PHP_EOL;
+        exit();
+    }
+
+
+    /* Put the articles in the correct category */
+    $statement = $connection->prepare("INSERT INTO oxobject2category (OXID, OXOBJECTID, OXCATNID, OXTIMESTAMP VALUES (?,?,?,?)");
+    if(!$statement) {
+        echo "Prepare failed: (" . $connection->errno . ")" . $connection->error . PHP_EOL;
+        exit();
+    }
+
+    /* bind statement */
+    $anotherRandomID = uniqid();
+    if(!$statement->bind_param("ssss", $anotherRandomID, $ID, $category, date("%Y-%M-%d %H:%M:%S"))) {
         echo "Binding parameters failed: (" . $statement->errno . ") " . $statement->error . PHP_EOL;
         exit();
     }
     if(!$statement->execute()) {
-        echo 'Error at import with ' . $entry . PHP_EOL;
-        echo "Line 57: Execute failed: (" . $statement->errno . ") " . $statement->error . PHP_EOL;
+        echo "Execute failed: (" . $statement->errno . ") " . $statement->error . PHP_EOL;
         exit();
     }
 }
